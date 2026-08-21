1108d81bad9e5bb37b73961d5eb52facc7a7aa69
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-21 17:04:02 +0300

    Testing

--- BusinessLogic/clsSupplierBL.cs
+++ BusinessLogic/clsSupplierBL.cs
@@ -10,42 +10,34 @@ namespace PharmacyApp.Business
         {
             return clsSupplierData.AddNewSupplier(supplierInfo);
         }
-
         public static bool IsPhoneExists(string phone, int excludeSupplierID = -1)
         {
             return clsSupplierData.IsPhoneExists(phone, excludeSupplierID);
         }
-
         public static bool IsEmailExists(string email, int excludeSupplierID = -1)
         {
             return clsSupplierData.IsEmailExists(email, excludeSupplierID);
         }
-
         public static stSupplierInfo GetSupplierByEmail(string email)
         {
             return clsSupplierData.GetSupplierByEmail(email);
         }
-
         public static bool UpdateSupplierInfo(stSupplierInfo supplierInfo)
         {
             return clsSupplierData.UpdateSupplierInfo(supplierInfo);
         }
-
         public static List<string> GetAllEmails()
         {
             return clsSupplierData.GetAllEmails();
         }
-
         public static List<stSupplierInfo> GetAllSuppliers()
         {
             return clsSupplierData.GetAllSuppliers();
         }
-
         public static List<string> GetAllPhones()
         {
             return clsSupplierData.GetAllPhones();
         }
-
         public static stSupplierInfo GetSupplierByPhone(string phone)
         {
             return clsSupplierData.GetSupplierByPhone(phone);
